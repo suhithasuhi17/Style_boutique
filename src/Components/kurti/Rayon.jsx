c3b6cf4199025b5dcd3076982b5241f66ab34ad1
suhitha <157236546+suhithasuhi17@users.noreply.github.com>
2025-04-20 21:32:31 +0530

    Update Rayon.jsx

--- src/Components/kurti/Rayon.jsx
+++ src/Components/kurti/Rayon.jsx
@@ -16,7 +16,7 @@ const Rayon = () => {
 
   return (
     <div className="min-h-screen bg-gray-100 py-10">
-      <Cart />
+      
       <h1 className="text-4xl font-bold text-center text-purple-700 mb-6">
         Rayon Collection
       </h1>
