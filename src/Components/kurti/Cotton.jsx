2d78b01917187fc1c97c8fd58587368d16cf35a4
suhitha <157236546+suhithasuhi17@users.noreply.github.com>
2025-04-20 21:31:52 +0530

    Update Cotton.jsx

--- src/Components/kurti/Cotton.jsx
+++ src/Components/kurti/Cotton.jsx
@@ -25,7 +25,7 @@ const Cotton = () => {
 
   return (
     <div className="min-h-screen bg-gray-100 py-10">
-      <Cart />
+      
       <h1 className="text-4xl font-bold text-center text-purple-700 mb-6">
         Cotton Collection
       </h1>
