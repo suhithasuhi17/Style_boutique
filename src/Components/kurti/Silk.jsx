47f1eff054fc169333ceea905bb356a6cf26224a
suhitha <157236546+suhithasuhi17@users.noreply.github.com>
2025-04-20 21:32:47 +0530

    Update Silk.jsx

--- src/Components/kurti/Silk.jsx
+++ src/Components/kurti/Silk.jsx
@@ -25,7 +25,7 @@ const Silk = () => {
 
   return (
     <div className="min-h-screen bg-gray-100 py-10">
-      <Cart />
+      
       <h1 className="text-4xl font-bold text-center text-purple-700 mb-6">
         Silk Collection
       </h1>
